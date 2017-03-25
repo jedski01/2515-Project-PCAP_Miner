7d701f529292bbaf867f1278675e30e773ad5673
jedski01 <jed.iquin.bcit@gmail.com>
2017-03-25 16:27:28 -0700

    added protocol enum for consistency

---
+++ src/pcap_packets/Protocol.java
@@ -0,0 +1,13 @@
+package pcap_packets;
+
+/**
+ * Protocol
+ * ACIT 2515 Activity name
+ * Enter a brief one sentence description of what this class is
+ *
+ * @author Jed Iquin A00790108
+ * @date 2017-03-25
+ */
+public enum Protocol {
+    ETHERNET, IPV4, IPV6, TCP, UDP
+}
